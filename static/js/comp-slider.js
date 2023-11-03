ded90312b7ff3976d8690ac5a9f11a017e22f093
Alex <36799721+MiSTeR1995@users.noreply.github.com>
2023-11-03 17:23:14 +0300

    conent update

--- static/js/comp-slider.js
+++ static/js/comp-slider.js
@@ -1,18 +1,18 @@
-const slider_first = document.querySelector('.slider-first');
-const slider_second = document.querySelector('.slider-second');
-const slider_third = document.querySelector('.slider-third');
+// const slider_first = document.querySelector('.slider-first');
+// const slider_second = document.querySelector('.slider-second');
+// const slider_third = document.querySelector('.slider-third');
 const btn = document.querySelector('#btt-button');
 
-document.querySelector('.slider-input-first').addEventListener('input', (e) => {
-    slider_first.style.setProperty('--position', `${e.target.value}%`);
-});
+// document.querySelector('.slider-input-first').addEventListener('input', (e) => {
+//     slider_first.style.setProperty('--position', `${e.target.value}%`);
+// });
 
-document.querySelector('.slider-input-second').addEventListener('input', (e) => {
-    slider_second.style.setProperty('--position', `${e.target.value}%`);
-});
-document.querySelector('.slider-input-third').addEventListener('input', (e) => {
-    slider_third.style.setProperty('--position', `${e.target.value}%`);
-});
+// document.querySelector('.slider-input-second').addEventListener('input', (e) => {
+//     slider_second.style.setProperty('--position', `${e.target.value}%`);
+// });
+// document.querySelector('.slider-input-third').addEventListener('input', (e) => {
+//     slider_third.style.setProperty('--position', `${e.target.value}%`);
+// });
 
 
 window.addEventListener('scroll', function () {
